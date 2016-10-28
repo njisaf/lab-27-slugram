45f28882af31dc496f7b6de672090d1210be502a
Nassir Isaf <njisaf@gmail.com>
2016-10-27 17:56:23 -0700

    Delete route failing; don't know how to pass _id into it except by making it a component, so moving on to class29

--- lab-nassir/app/view/home/home-controller.js
+++ lab-nassir/app/view/home/home-controller.js
@@ -2,9 +2,31 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', HomeController ];
+module.exports = ['$log', '$rootScope', 'galleryService', HomeController ];
 
-function HomeController($log){
-  $log.debug('init HomeController;');
-  this.oneAtATime = true;
+function HomeController($log, $rootScope, galleryService){
+  $log.debug('Initializing HomeController;');
+
+  this.galleries = [];
+
+  this.fetchGalleries = function() {
+    galleryService.fetchGalleries()
+    .then(galleries => {
+      this.galleries = galleries;
+    });
+  };
+
+  this.deleteGalleries = function() {
+    $log.debug('sfsfsfsfs', this.gallery);
+    galleryService.deleteGalleries(this.currentID)
+    .then(() => {
+      this.fetchGalleries();
+    });
+  };
+
+  this.fetchGalleries();
+  $rootScope.$on('$locationChangeSuccess', () => {
+    this.fetchGalleries();
+  });
+
 }
